58c7bfe77008b682eb26fa14059601a0a32f1729
Edson Marques <edsonmarquessc@gmail.com>
2023-09-19 21:48:06 -0300

    paper
    teste01

---
+++ php/conexao.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "regioesdobrasil";
+
+$conn = new PDO("mysql:host=$host;dbname=" . $dbname, $user, $pass);
+
+?>
+
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "javascript";
+
+$conexao = new PDO("mysql:host=$host;dbname=" . $dbname, $user, $pass);
+?>
